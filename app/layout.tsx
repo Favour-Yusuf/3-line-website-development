e6a03cbb3edc8bcfa698152a44a72d74ffe22870
Favour-Yusuf <favouryusuf45@gmail.com>
2025-05-06 16:34:13 +0100

    done again

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   title: "3line - Your Preferred Payment Partner",
   description:
     "We are transforming how financial institutions and businesses process payments with our secure, fast, and more accessible solutions for everyone.",
+    icons: {
+      icon: "/logo.png",
+    },
 }
 
 export default function RootLayout({
