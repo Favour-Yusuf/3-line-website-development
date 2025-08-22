466b199588d4a86ade622b8f02fb3994ee8f81eb
Favour-Yusuf <favouryusuf45@gmail.com>
2025-08-22 15:49:19 +0100

    updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
                     </Link>
                   </li>
                   <li>
-                    <Link href="#" className="hover:underline">
+                    <Link href="/our-team" className="hover:underline">
                       Careers
                     </Link>
                   </li>
@@ -230,7 +230,7 @@ export default function Footer() {
                   </Link>
                 </li>
                 <li>
-                  <Link href="#" className="hover:underline">
+                  <Link href="/our-team" className="hover:underline">
                     Careers
                   </Link>
                 </li>
