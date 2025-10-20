e56119ea7303acb1e4b524f13ded46ab7ac3af59
Favour-Yusuf <favouryusuf45@gmail.com>
2025-10-20 13:03:52 +0100

    client

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -9,7 +9,7 @@ export default async function PrivacyPolicyPage() {
   }
 
   return (
-    <main className="min-h-screen bg-[#EEF3FF] py-12 px-6">
+    <main className="min-h-screen bg-[#EEF3FF] py-28 px-6 ">
       <div className="max-w-3xl mx-auto bg-white rounded-2xl shadow-md p-8">
         <h1 className="text-3xl font-bold mb-4 text-gray-800">{data.title}</h1>
         <p className="text-sm text-gray-500 mb-6">
